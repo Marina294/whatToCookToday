b0876648db6a753ca3332b4d77aeebb1a071ef0a
Marina294 <69162947+Marina294@users.noreply.github.com>
2021-08-16 21:52:13 -0700

    recipe

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import { BiSearchAlt } from "react-icons/bi";
 import "./index.css";
 
 const APP_ID = "47915a59";
-const APP_KEY = process.env.REACT_APP_API_KEY;
+const APP_KEY = "daa28f2d2ccd74d661d0be4832236c41";
 
 
 const RecipeContainer = styled.div`
